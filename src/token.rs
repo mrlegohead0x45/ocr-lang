4e8c6365b9e40223be65e3cf7c12b11bd9c9e5a7
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2022-10-10 18:00:47 +0100

    Add Token enum

---
+++ src/token.rs
@@ -0,0 +1,83 @@
+/// Enum for tokens
+#[allow(unused)] // TODO: Use all of them!
+#[derive(Debug)]
+enum Token {
+    // Keywords
+    Class,
+    Public,
+    Private,
+    Inherits,
+    New,
+    EndClass,
+
+    Function,
+    ByVal, // annotations in function definition. byval is assumed if none given.
+    ByRef, // e.g. function foobar(x: byval, y: byref)
+    Return,
+    EndFunction,
+
+    Procedure,
+    EndProcedure,
+
+    If,
+    Then,
+    ElseIf,
+    Else,
+    EndIf,
+
+    Switch,
+    Case,
+    EndSwitch,
+
+    For,
+    To,
+    Next,
+
+    While,
+    EndWhile,
+
+    Do,
+    Until,
+
+    Global,
+    Array,
+
+    // Parens
+    LParen,
+    RParen,
+
+    LSquareParen,
+    RSquareParen,
+
+    // Logical operators
+    And,
+    Or,
+    Not,
+
+    EqTo,
+    Ne,
+    Lt,
+    Le,
+    Gt,
+    Ge,
+
+    Plus,
+    Minus,
+    Mul,
+    Div,      // regular division
+    Mod,      // Mod
+    FloorDiv, // DIV
+    Exp,      // ^
+
+    // Hold values
+    Identifier(String),
+    StringLiteral(String),
+    IntegerLiteral(i64),
+    FloatLiteral(f64),
+
+    // Misc
+    Colon,
+    Dot,
+    Comma,
+    AssignEq,
+}
