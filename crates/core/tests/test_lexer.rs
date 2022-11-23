1f2d22819f43b50e8e083a7d1ac9708cd09ed52f
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2022-11-23 12:34:26 +0000

    Continue tests

--- crates/core/tests/test_lexer.rs
+++ crates/core/tests/test_lexer.rs
@@ -1,5 +1,9 @@
-use paste::paste;
 use std::fs::File;
+use std::path::Path;
+
+use paste::paste;
+
+use core::Lexer;
 
 macro_rules! make_test {
     ($name: expr) => {
@@ -7,12 +11,20 @@ macro_rules! make_test {
             #[test]
             fn [< test_ $name >] (){
                 let source_filename = concat!(stringify!($name), ".ocr");
-                let mut source_file = File::open(source_filename).unwrap_or_else(|_| panic!("Could not open {}", source_filename));
-                let lexer = core::Lexer::new(Box::new(source_file), source_filename.to_string());
+                // e.g .../ocr-lang/crates/core/tests/data/lexer/filename.ocr
+                // .join()s needed for windows compatibility, as that knows whether to use / or \
+                let source_file_path = Path::new(env!("CARGO_MANIFEST_DIR"))
+                    .join("tests")
+                    .join("data")
+                    .join("lexer")
+                    .join(source_filename);
+
+                let mut source_file = File::open(&source_file_path).unwrap_or_else(|_| panic!("Could not open {}", source_file_path.display()));
+                let lexer = Lexer::new(Box::new(source_file), source_filename.to_string());
 
             }
         }
     };
 }
 
-// make_test!(hello_world);
+make_test!(hello_world);
